4aa27b6dc83b47b39ab1b1913d2d0d1a87ed976f
SalmanRAMc <hussainyred@gmail.com>
2024-03-10 13:47:42 +0200

    automated alias message.. thanks bash

--- models/base_model.py
+++ models/base_model.py
@@ -1,18 +1,36 @@
 import uuid
 from datetime import datetime
+
+
 class BaseModel():
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    # both args and kwargs indicate an unknown number
+    # of passed parameters. However kwargs makes use of parameters
+    # that have keywords in them (mostly used for dictionaries)
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['updated_at', 'created_at']:
+                        value = key.strptime("%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+                else:
+                    continue
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        # initialise a class either through dictionary input
+        # or manual initialisation
 
     def save(self):
         self.updated_at = datetime.now()
-        # save the storage
-    
+        # save the update time into storage
+
     def __str__(self):
+        # returns a string with object's name ID
+        # and the dictionary with the rest of its attributes
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
-    
+
     def to_dict(self):
         # Get the dictionary representation of the object's attributes
         result_dict = dict(self.__dict__)
