fd6f7fa9e2c73c03dfe22b6cd8054f714e81cac7
SalmanRAMc <hussainyred@gmail.com>
2024-03-10 11:54:17 +0200

    automated alias message.. thanks bash

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,25 @@ from datetime import datetime
 class BaseModel():
     def __init__(self):
         id = str(uuid.uuid4())
-        created_at = datetime.today()
-        updated_at = datetime.today()
+        created_at = datetime.now()
+        updated_at = datetime.now()
 
     def save(self):
-        self.updated_at = datetime.today()
+        self.updated_at = datetime.now()
         # save the storage
     
     def __str__(self):
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    
+    def to_dict(self):
+        # Get the dictionary representation of the object's attributes
+        result_dict = dict(self.__dict__)
+
+        # Add a new field "__class__" to store the object's name
+        result_dict['__class__'] = self.__class__.__name__
+
+        # Convert created_at and updated_at into their ISO Format
+        result_dict['created_at'] = self.created_at.isoformat()
+        result_dict['updated_at'] = self.updated_at.isoformat()
+
+        return result_dict
