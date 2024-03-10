9370c28d01c9d90239ca7b65e707c35f2d1abd32
SalmanRAMc <hussainyred@gmail.com>
2024-03-10 11:56:56 +0200

    automated alias message.. thanks bash

--- models/base_model.py
+++ models/base_model.py
@@ -2,9 +2,9 @@ import uuid
 from datetime import datetime
 class BaseModel():
     def __init__(self):
-        id = str(uuid.uuid4())
-        created_at = datetime.now()
-        updated_at = datetime.now()
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         self.updated_at = datetime.now()
